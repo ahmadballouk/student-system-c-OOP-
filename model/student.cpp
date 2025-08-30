a0797a56d0e11d80eda7ca4b220e60a2840b8e6f
AHMAD <ahmadballouk246@gmail.com>
2025-08-30 04:36:31 -0300

    Create Model BaseEntity

--- model/student.cpp
+++ model/student.cpp
@@ -1,37 +1,18 @@
 
+#include "BaseEntity.cpp"
 
-class Student 
+
+class Student : public BaseEntity
 {
     private : 
 
-        int id ;
-        string name;
-        int age  ;
-        string PhoneNumber ;  
+         
         double gpa ; 
 
     public : 
 
     //* setters
-        void setId (int id)
-        {
-            this -> id = id ; 
-        }
-
-     void setName (string name)
-        {
-            this ->name = name ; 
-        }
-
-         void setAge (int age)
-        {
-            this -> age = age ; 
-        }
-
-         void setPhoneNumber (string PhoneNumber)
-        {
-            this ->PhoneNumber = PhoneNumber ; 
-        }
+       
 
          void setGpa (double gpa)
         {
@@ -40,25 +21,7 @@ class Student
 
         //*getters
 
-        int getId ()
-        {
-            return id ; 
-        }
-
-     string getName ()
-        {
-            return name ; 
-        }
-
-         int  getAge ()
-        {
-           return age ;  
-        }
-
-         string getPhoneNumber ()
-        {
-          return PhoneNumber ;   
-        }
+     
 
          double getGpa ()
         {
--- model/teacher.cpp
+++ model/teacher.cpp
@@ -1,36 +1,19 @@
-class Teacher 
+
+#include "BaseEntity.cpp"
+
+
+
+class Teacher : public BaseEntity
 {
  private : 
 
-        int id ;
-        string name;
-        int age  ;
-        string PhoneNumber ;  
+         
         double salary  ; 
 
     public : 
 
     //* setters
-        void setId (int id)
-        {
-            this -> id = id ; 
-        }
-
-     void setName (string name)
-        {
-            this ->name = name ; 
-        }
-
-         void setAge (int age)
-        {
-            this -> age = age ; 
-        }
-
-         void setPhoneNumber (string PhoneNumber)
-        {
-            this ->PhoneNumber = PhoneNumber ; 
-        }
-
+       
          void setSalary (double salary)
         {
             this ->salary = salary ; 
@@ -38,25 +21,7 @@ class Teacher
 
         //*getters
 
-        int getId ()
-        {
-            return id ; 
-        }
 
-     string getName ()
-        {
-            return name ; 
-        }
-
-         int  getAge ()
-        {
-           return age ;  
-        }
-
-         string getPhoneNumber ()
-        {
-          return PhoneNumber ;   
-        }
 
          double getSalary ()
         {
